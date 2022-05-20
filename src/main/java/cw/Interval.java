cffa7531b1a30bb2f03baf02df0898efad2d925b
Sergey Kovalev <dip.deep@mail.ru>
2022-05-20 23:08:09 +0300

    task completed

--- src/main/java/cw/Interval.java
+++ src/main/java/cw/Interval.java
@@ -9,14 +9,15 @@ public class Interval {
         if (intervals == null) return 0;
         if (intervals.length == 0) return 0;
         Arrays.sort(intervals, Comparator.comparingInt(arr -> arr[0]));
+        int max = intervals[0][1];
         int sum = intervals[0][1] - intervals[0][0];
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i-1][1] < intervals[i][1]) {
-                if (intervals[i-1][1] > intervals[i][0]) {
-                    sum -= intervals[i-1][1] - intervals[i][1];
-                } else {
-                    sum += intervals[i][1] - intervals[i][0];
-                }
+            if (intervals[i][0] >= max) {
+                sum += intervals[i][1] - intervals[i][0];
+                max = intervals[i][1];
+            } else if (intervals[i][1] > max) {
+                sum -= max - intervals[i][1];
+                max = intervals[i][1];
             }
         }
         return sum;
